222b23338c982bc916e967c6e6dcc8ccc4d1d8ac
MarinaMyz <marynakravchenko77@gmail.com>
2024-02-22 12:26:31 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const sum = function (a, b) {
+  return a + b
+}
+sum(1, 2)
+const minus = function (a, b) {
+  return a - b
+}
+minus(3, 4)
+const mul = function (a, b) {
+  return a * b
+}
+mul(5, 6)
